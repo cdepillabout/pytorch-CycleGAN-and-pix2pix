3048038eab83ff4117698680f7a7b9273315a5cb
(cdep)illabout <cdep.illabout@gmail.com>
2018-11-26 11:27:36 +0900

    Remove stuff I don't need from the shell.nix file.

--- shell.nix
+++ shell.nix
@@ -12,24 +12,8 @@ let
       else nixpkgs;
   pkgs = import nixpkgsSrc { config = { allowUnfree = true; }; };
 in
-with pkgs;
 
-# let
-#   pythonEnv = pkgs.python3.buildEnv.override {
-#     extraLibs = with pkgs.python3Packages; [
-#       pytorch
-#     ];
-#     # ignoreCollisions = true;
-#   };
-# in
-# pkgs.stdenv.mkDerivation {
-#   name = "pytorch-env";
-#   buildInputs = [pythonEnv];
-#   shellHook = ''
-#     export LANG=en_US.UTF-8
-#     export PYTHONIOENCODING=UTF-8
-#   '';
-# }
+with pkgs;
 
 let
   torchfile = python3.pkgs.buildPythonPackage rec {
@@ -42,34 +26,12 @@ let
     };
   };
 
-  visdom = python3.pkgs.buildPythonPackage rec {
-    pname = "visdom";
-    version = "0.1.8.5";
-
-    src = python35.pkgs.fetchPypi {
-      inherit pname version;
-      sha256 = "0sqkd5c7b43q7n7xz4x0rby28hq222h550skw9q6v1aanvswrbz9";
-    };
-
-    propagatedBuildInputs = with python3.pkgs; [
-      pillow
-      pyzmq
-      numpy
-      scipy
-      requests
-      torchfile
-      tornado
-      websocket_client
-    ];
-  };
-
   pyWithPytorch = (python3.withPackages (ps: with ps; [
     dominate
     pip
     pytorch
     torchvision
     virtualenv
-    #visdom
 
     # visdom deps
     pillow
@@ -82,20 +44,6 @@ let
     websocket_client
   ]));
 in
-
-# pyWithPytorch.env.overrideAttrs (oldAttrs: {
-#   paths = oldAttrs.paths ++ [
-#     taglib
-#     openssl
-#     git
-#     libxml2
-#     libxslt
-#     libzip
-#     stdenv
-#     zlib
-#   ];
-
-# })
 
 mkShell {
   name = "my-env";
